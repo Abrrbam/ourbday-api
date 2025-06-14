cca51f43396dd0cd366d8c33ab3cfb739b6cea60
Abraham Putra Berman Simbolon <abrahamputrabs@gmail.com>
2025-06-15 00:01:06 +0700

    fix #7: add const dbhost

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,10 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const dbname = process.env.DB_NAME || "default_db";
+const dbhost = process.env.DB_HOST || "localhost";
 const dbuser = process.env.DB_USER || "default_user";
 const dbpassword = process.env.DB_PASSWORD || "default_password";
 const database = new Sequelize(dbname, dbuser, dbpassword, {
-    host: process.env.DB_HOST!,
+    host: dbhost,
     dialect: "mysql"
     })
 
