ab50ba884fa434f9b6c74d6324550b0a996d3833
Abraham Putra Berman Simbolon <abrahamputrabs@gmail.com>
2025-06-15 00:48:50 +0700

    test #1: console.log Environment Database

--- src/database/database.ts
+++ src/database/database.ts
@@ -7,6 +7,13 @@ const dbname = process.env.DB_NAME || "default_db";
 const dbhost = process.env.DB_HOST || "localhost";
 const dbuser = process.env.DB_USER || "default_user";
 const dbpassword = process.env.DB_PASSWORD || "default_password";
+
+console.log("DB_HOST:", dbhost);
+console.log("DB_USER:", dbuser);
+console.log("DB_PASSWORD:", dbpassword);
+console.log("DB_NAME:", dbname);
+
+
 const database = new Sequelize(dbname, dbuser, dbpassword, {
     host: dbhost,
     dialect: "mysql"
